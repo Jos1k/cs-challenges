a49f6e55ab6f153a1509634116d1ea617e5c2bdf
Ihor Hadzera <ihor.hadzera@additiv.ch>
2017-08-25 16:27:14 +0200

    added bidirectional array rotation

--- Preparation/Arrays/ArrayRotation/ArrayRotation.Tests/ArrayRotatorTest.cs
+++ Preparation/Arrays/ArrayRotation/ArrayRotation.Tests/ArrayRotatorTest.cs
@@ -6,13 +6,49 @@ namespace ArrayRotation.Tests
     public class ArrayRotatorTest
     {
         [TestMethod]
-        public void RotateArrayTest()
+        public void RotateArrayLeftTest()
         {
             var inputArray = new [] { 1, 2, 3, 4, 5, 6, 7 };
             var expectedResult = new[] { 3, 4, 5, 6, 7, 1, 2 };
             var rotateNumber = 2;
 
-            var outputArray = ArrayRotator.RotateArray(inputArray, rotateNumber);
+            var outputArray = ArrayRotator.RotateArrayLeft(inputArray, rotateNumber);
+
+            CollectionAssert.AreEqual(outputArray, expectedResult);
+        }
+
+        [TestMethod]
+        public void RotateArrayLeftFewFullTimesTest()
+        {
+            var inputArray = new[] { 1, 2, 3, 4, 5, 6, 7 };
+            var expectedResult = new[] { 3, 4, 5, 6, 7, 1, 2 };
+            var rotateNumber = 9;
+
+            var outputArray = ArrayRotator.RotateArrayLeft(inputArray, rotateNumber);
+
+            CollectionAssert.AreEqual(outputArray, expectedResult);
+        }
+
+        [TestMethod]
+        public void RotateArrayRightTest()
+        {
+            var inputArray = new[] { 1, 2, 3, 4, 5, 6, 7 };
+            var expectedResult = new[] { 6, 7, 1, 2, 3, 4, 5 };
+            var rotateNumber = 2;
+
+            var outputArray = ArrayRotator.RotateArrayRight(inputArray, rotateNumber);
+
+            CollectionAssert.AreEqual(outputArray, expectedResult);
+        }
+
+        [TestMethod]
+        public void RotateArrayRightFewFullTimesTest()
+        {
+            var inputArray = new[] { 1, 2, 3, 4, 5, 6, 7 };
+            var expectedResult = new[] { 6, 7, 1, 2, 3, 4, 5 };
+            var rotateNumber = 9;
+
+            var outputArray = ArrayRotator.RotateArrayRight(inputArray, rotateNumber);
 
             CollectionAssert.AreEqual(outputArray, expectedResult);
         }
--- Preparation/Arrays/ArrayRotation/ArrayRotation/ArrayRotator.cs
+++ Preparation/Arrays/ArrayRotation/ArrayRotation/ArrayRotator.cs
@@ -5,19 +5,40 @@ namespace ArrayRotation
 {
     public static class ArrayRotator
     {
-        public static int[] RotateArray(int[] array, int rotateNumber)
-        {
-            int[] shiftArray = array.Take(array.Length - rotateNumber).ToArray();
 
-            for (int i=0;i< rotateNumber; i++)
+        public static int[] RotateArrayLeft(int[] array, int rotateNumber)
+        {
+            rotateNumber = rotateNumber % array.Length;
+            int shift = array.Length - rotateNumber;
+            int[] shiftArray = array.Take(rotateNumber).ToArray();
+
+            for (int i=0;i< shift; i++)
             {
                 array[i] = array[i+ shiftArray.Length];
             }
 
             for (int i = 0; i < shiftArray.Length; i++)
             {
-                array[i + rotateNumber] = shiftArray[i];
+                array[i + shift] = shiftArray[i];
             }
+            return array;
+        }
+
+        public static int[] RotateArrayRight(int[]array, int rotateNumber)
+        {
+            rotateNumber = rotateNumber % array.Length;
+            int shift = array.Length - rotateNumber;
+            int[] shiftArray = array.Skip(shift).Take(rotateNumber).ToArray();
+
+            for (int i = shift-1; i >=0 ; i--)
+            {
+                array[i + rotateNumber] = array[i];
+            }
+            for (int i = 0; i < shiftArray.Length; i++)
+            {
+                array[i] = shiftArray[i];
+            }
+
             return array;
         }
     }
