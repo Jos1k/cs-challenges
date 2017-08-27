c3c8b068b5f67758e1a1c9b816d43cb536ed28ad
Ihor Hadzera <josik4ke@gmail.com>
2017-08-27 16:38:53 +0200

    added implementation GCD

---
+++ Preparation/Numbers/GreaterCommonDividor/EuclideanAlg/CGDCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuclideanAlg
+{
+	public class CGDCalculator {
+		public static int GetCGD( int a, int b ) {
+			return 0;
+		}
+	}
+}
