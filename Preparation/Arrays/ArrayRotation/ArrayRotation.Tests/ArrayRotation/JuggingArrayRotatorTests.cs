fc5dfacd7a28944167ed323a3528704b22138bde
Ihor Hadzera <josik4ke@gmail.com>
2017-08-28 22:11:31 +0200

    first implementation of juggin rotate

---
+++ Preparation/Arrays/ArrayRotation/ArrayRotation.Tests/ArrayRotation/JuggingArrayRotatorTests.cs
@@ -0,0 +1,34 @@
+﻿using ArrayRotation.Implementation;
+using NUnit.Framework;
+
+namespace ArrayRotation.Tests.ArrayRotation {
+	[TestFixture]
+	public class JuggingArrayRotatorTests {
+
+		private IArrayRotator _jugginArrayRotator;
+		
+		[OneTimeSetUp]
+		public void Setup() {
+			_jugginArrayRotator = new JuggingArrayRotator();
+		}
+
+		[TestCaseSource( "RotateLeftPositive_TestCases" )]
+		public void RotateLeft_Positive( int[] inputArray, int rotateTimes, int[] expectedResult) {
+			var actulResult = _jugginArrayRotator.RotateLeft( inputArray, rotateTimes );
+			CollectionAssert.AreEqual( expectedResult, actulResult );
+		}
+
+		private static object[] RotateLeftPositive_TestCases = new object[]{
+			//new object[] { 
+			//	new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }, 
+			//	3, 
+			//	new int[] { 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3 }
+			//},
+			new object[] { 
+				new int[] { 1, 2, 3, 4, 5, 6, 7 }, 
+				2, 
+				new int[] { 3, 4, 5, 6, 7, 1, 2 }
+			}
+		};
+	}
+}
---
+++ Preparation/Arrays/ArrayRotation/ArrayRotation/Implementation/JuggingArrayRotator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayRotation.Implementation {
+	public class JuggingArrayRotator : IArrayRotator {
+		public int[] RotateLeft( int[] inputArray, int rotateTimes ) {
+			rotateTimes = rotateTimes % inputArray.Length;
+			int gcd = GetCGD( inputArray.Length, rotateTimes );
+			int insertPointer = inputArray.Length - rotateTimes;
+			for( int rotateIndex = 0; rotateIndex < rotateTimes; rotateIndex++ ) {
+				int temp = inputArray[rotateIndex];
+
+				for( int shiftIndex = rotateIndex; shiftIndex < insertPointer; shiftIndex += gcd ) {
+					inputArray[shiftIndex] = inputArray[shiftIndex + gcd];
+				}
+				inputArray[insertPointer] = temp;
+				insertPointer++;
+			}
+			return inputArray;
+		}
+
+		public int[] RotateRight( int[] inputArray, int rotateTimes ) {
+			throw new NotImplementedException();
+		}
+
+		private static int GetCGD( int a, int b ) {
+			int temp;
+			while( b != 0 ) {
+				temp = b;
+				b = a % b;
+				a = temp;
+			}
+			return a;
+		}
+	}
+}
