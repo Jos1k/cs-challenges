931dc8fe09b129743a65c71665fcd5dc828672cd
Ihor Hadzera <josik4ke@gmail.com>
2017-08-28 23:10:24 +0200

    implemented jugging arrat

--- Preparation/Arrays/ArrayRotation/ArrayRotation/Implementation/JuggingArrayRotator.cs
+++ Preparation/Arrays/ArrayRotation/ArrayRotation/Implementation/JuggingArrayRotator.cs
@@ -7,29 +7,49 @@ using System.Threading.Tasks;
 namespace ArrayRotation.Implementation {
 	public class JuggingArrayRotator : IArrayRotator {
 		public int[] RotateLeft( int[] inputArray, int rotateTimes ) {
-			rotateTimes = rotateTimes % inputArray.Length;
 
-			int gcd = GetCGD( inputArray.Length, rotateTimes );
-
-			if( gcd > 1 ) {
-				int insertPointer = inputArray.Length - rotateTimes;
-				for( int rotateIndex = 0; rotateIndex < rotateTimes; rotateIndex++ ) {
-					int temp = inputArray[rotateIndex];
-					for( int shiftIndex = rotateIndex; shiftIndex < insertPointer; shiftIndex += gcd ) {
-						inputArray[shiftIndex] = inputArray[shiftIndex + gcd];
-					}
-					inputArray[insertPointer] = temp;
-					insertPointer++;
+			int i, j, k, temp;
+			int n = inputArray.Length;
+			for( i = 0; i < GetCGD( rotateTimes, n ); i++ ) {
+				temp = inputArray[i];
+				j = i;
+				while( 1 != 0 ) {
+					k = j + rotateTimes;
+					if( k >= n )
+						k = k - n;
+					if( k == i )
+						break;
+					inputArray[j] = inputArray[k];
+					j = k;
 				}
-			} else {
-				for( int rotateIndex = 0; rotateIndex < rotateTimes; rotateIndex++ ) {
-					int temp = inputArray[0];
-					for( int i = 0; i < inputArray.Length - 1; i++ ) {
-						inputArray[i] = inputArray[i + 1];
-					}
-					inputArray[inputArray.Length - 1] = temp;
-				}
+				inputArray[j] = temp;
 			}
+
+
+
+			//rotateTimes = rotateTimes % inputArray.Length;
+
+			//int gcd = GetCGD( inputArray.Length, rotateTimes );
+
+			//if( gcd > 1 ) {
+			//	int insertPointer = inputArray.Length - rotateTimes;
+			//	for( int rotateIndex = 0; rotateIndex < rotateTimes; rotateIndex++ ) {
+			//		int temp = inputArray[rotateIndex];
+			//		for( int shiftIndex = rotateIndex; shiftIndex < insertPointer; shiftIndex += gcd ) {
+			//			inputArray[shiftIndex] = inputArray[shiftIndex + gcd];
+			//		}
+			//		inputArray[insertPointer] = temp;
+			//		insertPointer++;
+			//	}
+			//} else {
+			//	for( int rotateIndex = 0; rotateIndex < rotateTimes; rotateIndex++ ) {
+			//		int temp = inputArray[0];
+			//		for( int i = 0; i < inputArray.Length - 1; i++ ) {
+			//			inputArray[i] = inputArray[i + 1];
+			//		}
+			//		inputArray[inputArray.Length - 1] = temp;
+			//	}
+			//}
 			return inputArray;
 		}
 
