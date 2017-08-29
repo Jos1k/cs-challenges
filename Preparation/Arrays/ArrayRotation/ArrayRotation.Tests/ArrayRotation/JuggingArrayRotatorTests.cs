11fc9a9eda040c853b1d4d8e3e861e1e967127f6
Ihor Hadzera <ihor.hadzera@additiv.ch>
2017-08-29 17:49:10 +0200

    added rotate juggling right and tests

--- Preparation/Arrays/ArrayRotation/ArrayRotation.Tests/ArrayRotation/JuggingArrayRotatorTests.cs
+++ Preparation/Arrays/ArrayRotation/ArrayRotation.Tests/ArrayRotation/JuggingArrayRotatorTests.cs
@@ -23,6 +23,13 @@ namespace ArrayRotation.Tests.ArrayRotation
             CollectionAssert.AreEqual(expectedResult, actualResult);
         }
 
+        [TestCaseSource(nameof(RotateRightPositiveTestCases))]
+        public void RotateRight_Positive(int[] inputArray, int rotateTimes, int[] expectedResult)
+        {
+            var actualResult = _arrayRotator.RotateRight(inputArray, rotateTimes);
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+        }
+
         private static readonly object[] RotateLeftPositiveTestCases = {
             new object[] {
                 new [] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
@@ -33,6 +40,31 @@ namespace ArrayRotation.Tests.ArrayRotation
                 new [] { 1, 2, 3, 4, 5, 6, 7 },
                 2,
                 new [] { 3, 4, 5, 6, 7, 1, 2 }
+            },
+            new object[]
+            {
+                new int[0],
+                0,
+                new int[0]
+            },
+            new object[]
+            {
+                new []{ 1, 2, 3, 4, 5, 6, 7 },
+                0,
+                new []{ 1, 2, 3, 4, 5, 6, 7 },
+            }
+        };
+
+        private static readonly object[] RotateRightPositiveTestCases = {
+            new object[] {
+                new [] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+                3,
+                new [] { 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9 }
+            },
+            new object[] {
+                new [] { 1, 2, 3, 4, 5, 6, 7 },
+                2,
+                new [] { 6, 7, 1, 2, 3, 4, 5 }
             },
             new object[]
             {
--- Preparation/Arrays/ArrayRotation/ArrayRotation/Implementation/JuggingArrayRotator.cs
+++ Preparation/Arrays/ArrayRotation/ArrayRotation/Implementation/JuggingArrayRotator.cs
@@ -3,7 +3,7 @@
 namespace ArrayRotation.Implementation {
 	public class JuggingArrayRotator : IArrayRotator {
 		public int[] RotateLeft( int[] outputArray, int rotateTimes ) {
-		    int n = outputArray.Length;
+			int n = outputArray.Length;
 			for(int i = 0; i < GetCGD( rotateTimes, n ); i++ ) {
 				int temp = outputArray[i];
 				var j = i;
@@ -21,12 +21,35 @@ namespace ArrayRotation.Implementation {
 			return outputArray;
 		}
 
-		public int[] RotateRight( int[] inputArray, int rotateTimes ) {
-			throw new NotImplementedException();
+		public int[] RotateRight( int[] inputArray, int rotateTimes )
+		{
+		    if (inputArray.Length == 0 || rotateTimes == 0)
+		    {
+		        return inputArray;
+		    }
+		    int arraySize = inputArray.Length;
+			rotateTimes = rotateTimes % arraySize;
+		    for (int i = 1; i <= GetCGD(rotateTimes, arraySize);i++)
+		    {
+		        int temp = inputArray[arraySize - i];
+		        int shiftTo = arraySize - i;
+		        while (true)
+		        {
+		            int shiftFrom = shiftTo - rotateTimes;
+		            if (shiftFrom < 0)
+		                shiftFrom += arraySize;
+		            if (shiftFrom == arraySize - i)
+		                break;
+		            inputArray[shiftTo] = inputArray[shiftFrom];
+		            shiftTo = shiftFrom;
+		        }
+		        inputArray[shiftTo] = temp;
+		    }
+		    return inputArray;
 		}
 
 		private static int GetCGD( int a, int b ) {
-		    while( b != 0 ) {
+			while( b != 0 ) {
 				var temp = b;
 				b = a % b;
 				a = temp;
